1f907d521f4516bcdd2efdc141e7661ec998af7e
Xian Li <xnslx89@gmail.com>
2020-06-01 22:41:57 -0500

    lyric details

--- src/LyricDetail/LyricDetail.js
+++ src/LyricDetail/LyricDetail.js
@@ -2,24 +2,27 @@ import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
 const LyricDetail = (props) => {
-    console.log(props)
+    // console.log(props)
 
     const trackId = props.match.params.id;
-    const [lyricDetail, setLyricDetail] = useState([]);
+    const [lyricDetail, setLyricDetail] = useState({});
     const [loading, setLoading] = useState(false);
+
     useEffect(() => {
-        const fetchLyricDetail = async() => {
-            setLoading(true)
-            const res = await axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${trackId}&apikey=2fb0796550ae4d881460ef2cbd2f61bc`)
-            setLyricDetail({lyricDetail: res.data.message.body.lyrics})
-            setLoading(false)
-        }
-        fetchLyricDetail()
+        setLoading(true)
+        axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${trackId}&apikey=2fb0796550ae4d881460ef2cbd2f61bc`)
+            .then(res => {
+                setLyricDetail({lyricDetail:res.data.message.body.lyrics})
+            })
+            .catch(error => console.log(error))
     }, [trackId])
+    
+    
+    console.log(lyricDetail)
 
     return (
         <div>
-            
+            <p>{lyricDetail.lyrics_body}</p>
         </div>
     )
 };
