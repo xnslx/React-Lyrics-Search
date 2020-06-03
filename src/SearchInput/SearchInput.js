1c344ba76ea4928f5ccf23832639cc29883f615c
Xian Li <xnslx89@gmail.com>
2020-06-02 23:30:14 -0500

    song title to get lyrics

--- src/SearchInput/SearchInput.js
+++ src/SearchInput/SearchInput.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const SearchInput = () => {
+    
+    const [query, setQuery] = useState('');
+
+    useEffect(() => {
+        axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=${query}&apikey=2fb0796550ae4d881460ef2cbd2f61bc`)
+            .then(res => {
+                console.log(res)
+            })
+    })
     return (
         <form>
-            <input type='text' placeholder='song title...'/>
+            <input 
+                type='text' 
+                placeholder='song title...'
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+            />
             <button>Get Track Lyrics</button>
         </form>
     )
