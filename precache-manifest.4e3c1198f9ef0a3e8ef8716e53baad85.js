51416e96a5dd973d34681ee255c6a7cc1f58923f
Xian Li <xnslx89@gmail.com>
2020-06-11 23:03:04 -0500

    Updates

---
+++ precache-manifest.4e3c1198f9ef0a3e8ef8716e53baad85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec8ec3f6a14250b56e35a3e981e5a6af",
+    "url": "/React-Lyrics-Search/index.html"
+  },
+  {
+    "revision": "dadc9bbc144a6f5c058b",
+    "url": "/React-Lyrics-Search/static/css/main.cef537aa.chunk.css"
+  },
+  {
+    "revision": "6d4d705667916f7797d0",
+    "url": "/React-Lyrics-Search/static/js/2.5d428d63.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Lyrics-Search/static/js/2.5d428d63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dadc9bbc144a6f5c058b",
+    "url": "/React-Lyrics-Search/static/js/main.7358bf9b.chunk.js"
+  },
+  {
+    "revision": "0ad6cac9fce47317f8e0",
+    "url": "/React-Lyrics-Search/static/js/runtime-main.b00b14f0.js"
+  }
+]);
